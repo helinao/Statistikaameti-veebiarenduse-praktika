3895f2bbb119cea0c9abb0b6e0bfb78cf52f800b
helinao <ohtla.helina@gmail.com>
2026-03-07 12:23:21 +0200

    add checking if answer is correct. giving feedback

--- viktoriin_Helina_Ohtla/src/components/Question.tsx
+++ viktoriin_Helina_Ohtla/src/components/Question.tsx
@@ -1,23 +1,35 @@
 import type { IQuestion } from "../questions/questionsInterface";
+import { useState } from "react";
 
 type QuestionProps = {
     question: IQuestion;
 };
 
 const Question = ({ question }: QuestionProps) => {
+    const [feedback, setFeedback] = useState("");
+    const checkAnswer = (option: string) => {
+        if (option === question.correctAnswer) {
+            setFeedback("Õige vastus!");
+        } else {
+            setFeedback("Vale vastus.");
+        }
+    };
+
     return (
         <div>
             <h2>{question.question}</h2>
             <ul>
                 {question.options.map((option) => (
                     <div key={option}>
-                        <button>
+                        <button onClick={() => checkAnswer(option)}>
                             {option}
                         </button>
                     </div>
                 ))}
             </ul>
+            <h3>{feedback}</h3>
         </div>
+
     );
 };
 
